d24e49eb23057ce8f7e12c46ec51aeb42d5662f8
vasilogi <jean.vasil@gmailcom>
2022-01-22 18:19:31 +0100

    measure temperature and humidity

---
+++ main.py
@@ -0,0 +1,29 @@
+# Read temperature and humidity data with an ESP32
+
+from machine import Pin
+from time import sleep
+import dht
+
+# define the sampling rate
+rate = 2  # get data every 2 seconds
+
+# define a dht object on the specified data pin
+sensor = dht.DHT11(Pin(4))
+
+# infinite loop
+
+while True:
+    try:
+        # delay
+        sleep(rate)
+        # measure
+        sensor.measure()
+        # save readings
+        T = sensor.temperature()
+        h = sensor.humidity()
+        # print readings
+        print('temperature: %.2f' % T)
+        print('humidity: %.2f' % h)
+    except OSError as e:
+        # System-related error
+        print('Failed to read sensor...')
